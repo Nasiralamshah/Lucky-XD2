3006437e8c1291e35418be32033506c07a9ca42b
Nasiralamshah <na1122gangster@gmail.com>
2025-07-28 19:11:11 +0500

    Update settings.js

--- settings.js
+++ settings.js
@@ -6,7 +6,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "",
+SESSION_ID: process.env.SESSION_ID || "lucky~6cFTTSaY#pCwvGSlLHBFeeO9cedF498oAkbuXA-XzuO7oYEEaxBU",
 // add your Session Id make sure it starts with lucky~
 PREFIX: process.env.PREFIX || ".",
 // add your prifix for bot   
@@ -68,12 +68,11 @@ LINK_WHITELIST: "youtube.com,github.com",
           DELETE_LINKS: process.env.DELETE_LINKS || "false",
 // automatic delete links witho remove member 
 
-          OWNER_NUMBER: process.env.OWNER_NUMBER || "",
+          OWNER_NUMBER: process.env.OWNER_NUMBER || "923305185872",
 // add your bot owner number
 
-OWNER_NAME: process.env.OWNER_NAME || "",
-// add bot owner name
-
+OWNER_NAME: process.env.OWNER_NAME || "mr thanks",
+// add bot owner na
               DESCRIPTION: process.env.DESCRIPTION || "*© Powered By Lucky Tech Hub*",
 // add bot owner name    
 
